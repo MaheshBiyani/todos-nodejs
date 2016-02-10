00ddf718eaf0933b86442b7aa807beee7891e9a2
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-10 15:33:38 +0530

    added query parameter

---
+++ variable-playground.js
@@ -0,0 +1,21 @@
+/*var person={
+	name : 'mahesh',
+	age :20
+}
+
+function updatePerson(obj){
+	obj.age=25;
+}
+updatePerson(person);
+console.log(person);*/
+
+var arr= [10,17,34]
+
+function updateArray( obj){
+	obj=[78,59];
+	debugger;
+
+}
+
+updateArray(arr);
+console.log(arr);
