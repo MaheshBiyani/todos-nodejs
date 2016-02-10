b4b01aed40bbd94c7e072200fa7bb78e3a371667
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-10 15:31:25 +0530

    added query parameter

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/', function (req, res) {
 	res.send('To do API root');
 })
 
-app.get('/todos' , function ( req, resp) {
+/*app.get('/todos' , function ( req, resp) {
 	resp.json(todos);
 });
-
+*/
 app.get('/todos/:id', function (req, res){
 
 	var todoId = parseInt(req.params.id);
@@ -37,6 +37,24 @@ app.get('/todos/:id', function (req, res){
 	}
 	
 });
+
+
+app.get('/todos', function (req , res) {
+
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed==='true'){
+		filteredTodos=_.where(filteredTodos ,{completed : true});
+		console.log(filteredTodos);	
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed==='false') {
+		
+		filteredTodos = _.where (filteredTodos ,{completed : false});
+		console.log(filteredTodos);
+
+	}
+	res.json(filteredTodos);
+})
 
 
 app.post('/todos', function (req, resp){
