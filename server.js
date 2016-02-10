3feb2ed07c28de5cde349408ac8c87297d710712
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-10 17:05:58 +0530

    added q query paramete

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/todos/:id', function (req, res){
 });
 
 
-app.get('/todos', function (req , res) {
+/*app.get('/todos', function (req , res) {
 
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -52,6 +52,28 @@ app.get('/todos', function (req , res) {
 		filteredTodos = _.where (filteredTodos ,{completed : false});
 		console.log(filteredTodos);
 
+	}
+	res.json(filteredTodos);
+})
+*/
+app.get('/todos', function (req , res) {
+
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed==='true'){
+		filteredTodos=_.where(filteredTodos ,{completed : true});
+		console.log(filteredTodos);	
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed==='false') {	
+		filteredTodos = _.where (filteredTodos ,{completed : false});
+		console.log(filteredTodos);
+
+	}
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ){
+		console.log(queryParams);
+		filteredTodos = _.filter(filteredTodos , function (todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) >-1;
+		})
 	}
 	res.json(filteredTodos);
 })
